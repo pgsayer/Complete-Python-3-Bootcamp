31865c6b50d245ba63be47c995c76956cb0a1b50
phils <philip.g.sayer@gmail.com>
2021-06-06 17:10:37 +0100

    Finished lecture 78

--- 06-Modules and Packages/lectures/myprogram.py
+++ 06-Modules and Packages/lectures/myprogram.py
@@ -1,3 +1,6 @@
-from mymodule import my_func
+from MyMainPackage import some_main_script          # Import from the main package
+from MyMainPackage.SubPackage import mysubscript    # Import from the sub package
 
-my_func()
+some_main_script.report_main()
+
+mysubscript.sub_report()
